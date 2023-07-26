8abb99c8a9d8bb07604f77b24704036ad7efd8a7
Swtpea1516 <134784015+Swtpea1516@users.noreply.github.com>
2023-07-26 17:28:01 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,20 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+  let whoR = who[Math.floor(Math.random() * who.length)];
+  let actionR = action[Math.floor(Math.random() * action.length)];
+  let whatR = what[Math.floor(Math.random() * what.length)];
+  let whenR = when[Math.floor(Math.random() * when.length)];
+  document.getElementById("exuse").innerHTML =
+    whoR + " " + actionR + " " + whatR + " " + whenR;
 };
