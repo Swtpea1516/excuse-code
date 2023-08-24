3b40124afc1bebb35e7156e4ffcece84baabb000
Swtpea1516 <134784015+Swtpea1516@users.noreply.github.com>
2023-08-24 16:21:19 +0000

    all done

--- src/app.js
+++ src/app.js
@@ -7,8 +7,15 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
+  let img = document.getElementById("img");
+  let pics = [
+    "https://www.science.org/do/10.1126/science.aav0458/abs/0808_dogPee_16x9.jpg",
+    "https://t3.ftcdn.net/jpg/02/39/42/00/360_F_239420088_yOXhuDymb8IBbggLrv8j8YZkICw934br.jpg",
+    "https://people.com/thmb/6KHcx7O0J3Gwubg8b_c05YmnWdU=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc():focal(999x0:1001x2)/tmnt2-ff-003-2000-e2e7c744347046e8b4de53b25cdd2b00.jpg",
+    "https://t3.ftcdn.net/jpg/03/49/32/28/360_F_349322868_pnh7gwrSXKmbCuu6ebDPjUjRi8QQ6uJm.jpg"
+  ];
   let who = ["The dog", "My grandma", "His turtle", "My bird"];
-  let action = ["ate", "peed", "crushed", "broke"];
+  let action = ["ate", "peed on", "crushed", "broke"];
   let what = ["my homework", "the keys", "the car"];
   let when = [
     "before the class",
@@ -17,10 +24,13 @@ window.onload = function() {
     "during my lunch",
     "while I was praying"
   ];
-  let whoR = who[Math.floor(Math.random() * who.length)];
+  let randomIdx = Math.floor(Math.random() * who.length);
+  let whoR = who[randomIdx];
+  let picsR = pics[randomIdx];
   let actionR = action[Math.floor(Math.random() * action.length)];
   let whatR = what[Math.floor(Math.random() * what.length)];
   let whenR = when[Math.floor(Math.random() * when.length)];
+  img.src = picsR;
   document.getElementById("excuse").innerHTML =
     whoR + " " + actionR + " " + whatR + " " + whenR;
 };
